cb13b65d4edd43c5050eee267076f7ef6e430be6
python_tanuki <araki130710@gmail.com>
2024-04-02 23:17:13 +0900

    add logging and faker package

--- client.py
+++ client.py
@@ -1,35 +1,52 @@
+import logging
+import os
 import socket
 import sys
 
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-server_address = './server_file'
-client_address = './client_file'
+client_address = '/tmp/client_socket_file'
+server_address = '/tmp/server_socket_file'
 
-print('try to have connection to {}'.format(client_address))
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+logging.basicConfig(filename="local_chat_messenger_client.log", level=logging.INFO)
+logger = logging.getLogger()
+
+try:
+    os.unlink(client_address)
+    logger.info("Unlink {}".format(client_address))
+except FileNotFoundError:
+    logger.info("You haven't need to unlink client_address: {}".format(client_address))
+
+sock.bind(client_address)
+logger.info("Bind client to {}".format(client_address))
 
 try:
     sock.connect(server_address)
-except socket.error as err:
-    print(err)
+except socket.error as error:
+    logger.error("Failed to connect with server.")
     sys.exit(1)
 
-while True:
-    try:
-        while True:
-            message = input()
-            sock.sendall(message.encode())
-            sock.settimeout(2)
-            data = str(sock.recv(32))
-            if data:
-                print('Server response: ' + data)
+try:
+    while True:
+        print("Please input something...!")
+        client_input = input()
+        encoded_client_input = client_input.encode()
+        sock.sendall(encoded_client_input)
+        # sock.settimeout(2)
+
+        try:
+            client_response = sock.recv(4096).decode('utf-8')
+            if client_response:
+                logger.info("Server response: {}".format(client_response))
+                print("Server response: {}".format(client_response))
+                print("Try again...!")
             else:
                 break
-
-    except socket.timeout:
-        print('Socket timeout, ending listening for server messages')
-
-    finally:
-        print('Close socket.')
-        sock.close()
-        break
+        except TimeoutError:
+            logger.error("Timeout error occurs.")
+            break
+finally:
+    print("Closing connection...")
+    logger.info("Closed socket.")
+    sock.close()
 
--- server.py
+++ server.py
@@ -1,38 +1,49 @@
+import logging
 import os
-import time
 import socket
 
+from faker import Faker
+
+server_address = '/tmp/server_socket_file'
+
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-address = './server_file'
+
+logging.basicConfig(filename="local_chat_messenger_server.log", level=logging.INFO)
+logger = logging.getLogger()
 
 try:
-    os.unlink(address)
+    os.unlink(server_address)
+    logger.info("Unlink {}".format(server_address))
 except FileNotFoundError:
-    pass
+    logger.info("You haven't need to unlink server_address: {}".format(server_address))
 
-sock.bind(address)
+sock.bind(server_address)
+logger.info("Bind server to {}".format(server_address))
+
 sock.listen(1)
 
-while True:
-    connection, client_address = sock.accept()
-    try:
-        print('connection from', client_address)
-        while True:
-            data = connection.recv(16)
-            decoded = data.decode('utf-8')
-            print('Recieved: ' + decoded)
-            if data:
-                decoded = 'ServerProcessing' + decoded
-                
-                connection.sendall(decoded.encode())
-            else:
-                print('no data was sent.')
-                break
-    finally:
-        print('Close connection.')
-        connection.close()
-        break
+try:
+    while True:
+        connection, client_address = sock.accept()
+        logger.info("Accept connection from {}".format(client_address))
+        message_from_client = connection.recv(4096).decode('utf-8')
+        print(message_from_client)
 
+        while True:
+            if message_from_client:
+                logger.info("Had a message (decoded by utf-8): {}".format(message_from_client))
+                fake = Faker()
+                response = fake.text()
+                logger.info("Generate fake response: {}".format(response))
+                connection.sendall(response.encode())
+                logger.info("Sent back encoded response for client")
+
+            else:
+                logger.info("Had a no message from client.")
+                break
+finally:
+    logger.info("Closing connection...")
+    sock.close()
 
 
 
